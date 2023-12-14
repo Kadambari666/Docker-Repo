7e6a890e2a03ebe169ef25734e479bfda685b854
Kadambari666 <126493809+Kadambari666@users.noreply.github.com>
2023-12-14 15:23:00 +0530

    Add files via upload

---
+++ TryWithResourcesExample.java
@@ -0,0 +1,19 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResourcesExample {
+    public static void main(String[] args) {
+        // Example: Using try-with-resources to automatically close resources
+        try (BufferedReader reader = new BufferedReader(new FileReader("example.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading from the file: " + e.getMessage());
+        }
+    }
+}
